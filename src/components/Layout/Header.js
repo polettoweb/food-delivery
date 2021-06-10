d2913878d1b36f58ec4cb86c4c0efb942ef67979
Marco Poletto <polettoweb@gmail.com>
2021-06-10 14:24:12 +0200

    Added cart button with style

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,11 +1,13 @@
 import meals from "../../assets/meals.jpeg";
 import classes from "./Header.module.scss";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = props => {
   return (
     <>
       <header className={classes.header}>
         <h1>Food Delivery</h1>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={meals} alt="Table with food" />
