19c312f18cb0351e180f4c7f9e2d52b473efe074
Marco Poletto <polettoweb@gmail.com>
2021-06-10 16:17:59 +0200

    Added modal and portal

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,24 @@
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.scss";
+
+const Cart = props => {
+  const cartItems = [{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map(
+    item => <li>{item.name}</li>
+  );
+
+  return (
+    <Modal>
+      <ul className={classes["cart-items"]}>{cartItems}</ul>
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>99.99</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
